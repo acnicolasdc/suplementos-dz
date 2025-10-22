44c762c5e44715f90b78406e79a4742323672bf2
danilo zapata <zapatamarindanilo@gmail.com>
2025-10-21 22:41:46 -0500

    finish_protein

--- products/creatinas.js
+++ products/creatinas.js
@@ -1,10 +1,11 @@
 const creatinaProducts = [
     {
         id: "crea_1",
-        name: "Creatina Premium",
+        name: "CREATINA PLATINUM QR 80 SERV",
         description: "Aumenta tu fuerza y rendimiento muscular",
-        price: 29.99,
-        image: "https://m.media-amazon.com/images/I/71f+UBXh2vL._AC_SL1500_.jpg",
+        price: 150000,
+        image: "https://suplementos.b-cdn.net/creatinas/creatina-platinum-qr-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales%20600x800/creatina-platinum-80serv-tabla-600x800.webp",
         category: "creatinas",
         benefits: [
             "Aumenta la fuerza muscular",
@@ -14,15 +15,156 @@ const creatinaProducts = [
     },
     {
         id: "crea_2",
-        name: "Creatina HCL",
+        name: "CREATINA MONOHIDRATADA CREAPURE DYMATIZE 60 SERV",
         description: "Mayor absorción y menos retención de agua",
-        price: 34.99,
-        image: "https://m.media-amazon.com/images/I/71f+UBXh2vL._AC_SL1500_.jpg",
+        price: 120000,
+        image: "https://suplementos.b-cdn.net/creatinas/creatina-monohidratada-creapure-dymatize-300g-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales%20600x800/creatina-dymatize-300g-600x800.webp",
         category: "creatinas",
         benefits: [
             "Mejor absorción",
             "Sin retención de agua",
             "Resultados rápidos"
         ]
-    }
+    },
+    {
+        id: "crea_3",
+        name: "CREATINE BASIC 60 SERV",
+        description: "Aumenta tu fuerza y rendimiento muscular",
+        price: 100000,
+        image: "https://suplementos.b-cdn.net/creatinas/Creatine-Basic-300g-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales%20600x800/creatina-basic-tabla-600x800.webp",
+        category: "creatinas",
+        benefits: [
+            "Aumenta la fuerza muscular",
+            "Mejora el rendimiento deportivo",
+            "Recuperación más rápida"
+        ]
+    },
+    {
+        id: "crea_4",
+        name: "CREATINA ON 120 SERV",
+        description: "Mayor absorción y menos retención de agua",
+        price: 180000,
+        image: "https://suplementos.b-cdn.net/creatinas/creatina-on-120-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/creatina-on-600-gramos-tabla-500x500.webp",
+        category: "creatinas",
+        benefits: [
+            "Mejor absorción",
+            "Sin retención de agua",
+            "Resultados rápidos"
+        ]
+    },
+    {
+        id: "crea_5",
+        name: "CREATINA IRON 100 SERV",
+        description: "Aumenta tu fuerza y rendimiento muscular",
+        price: 110000,
+        image: "https://suplementos.b-cdn.net/creatinas/creatina-iron-10serv-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/creatina-iron-100servicios-tabla-500.webp",
+        category: "creatinas",
+        benefits: [
+            "Aumenta la fuerza muscular",
+            "Mejora el rendimiento deportivo",
+            "Recuperación más rápida"
+        ]
+    },
+    {
+        id: "crea_6",
+        name: "CREATINA ON 60 SERV",
+        description: "Mayor absorción y menos retención de agua",
+        price: 110000,
+        image: "https://suplementos.b-cdn.net/creatinas/creatina-on-60serv-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/creatina-on-tabla-500.webp",
+        category: "creatinas",
+        benefits: [
+            "Mejor absorción",
+            "Sin retención de agua",
+            "Resultados rápidos"
+        ]
+    },
+    {
+        id: "crea_7",
+        name: "CREA STACK 60 SERV",
+        description: "Aumenta tu fuerza y rendimiento muscular",
+        price: 140000,
+        image: "https://suplementos.b-cdn.net/creatinas/crea-stack-60-serv-red-berries-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/creastack-tabla-500.webp",
+        category: "creatinas",
+        benefits: [
+            "Aumenta la fuerza muscular",
+            "Mejora el rendimiento deportivo",
+            "Recuperación más rápida"
+        ]
+    },
+    {
+        id: "crea_8",
+        name: "CREATINA ON 3 EN 1 40 SERV",
+        description: "Mayor absorción y menos retención de agua",
+        price: 105000,
+        image: "https://suplementos.b-cdn.net/creatinas/creatina-3en1-on-40-serv-orange-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales%20600x800/creatina-on-3-en-1-40serv-tabla-600x800.webp",
+        category: "creatinas",
+        benefits: [
+            "Mejor absorción",
+            "Sin retención de agua",
+            "Resultados rápidos"
+        ]
+    }, {
+        id: "crea_9",
+        name: "Creatina Premium",
+        description: "Aumenta tu fuerza y rendimiento muscular",
+        price: 110000,
+        image: "https://suplementos.b-cdn.net/creatinas/legacy-50s-fruitpunch-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/legacy-50serv-tabla-800.webp",
+            category: "creatinas",
+        benefits: [
+            "Aumenta la fuerza muscular",
+            "Mejora el rendimiento deportivo",
+            "Recuperación más rápida"
+        ]
+    },
+    {
+        id: "crea_10",
+        name: "Creatina HCL",
+        description: "Mayor absorción y menos retención de agua",
+        price: 135000,
+        image: "https://suplementos.b-cdn.net/creatinas/cell-tech-3lbs-fruit-punch-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/celltech-citrus-punch-500.webp",
+        category: "creatinas",
+        benefits: [
+            "Mejor absorción",
+            "Sin retención de agua",
+            "Resultados rápidos"
+        ]
+    },
+    {
+        id: "crea_11",
+        name: "Creatina HCL",
+        description: "Mayor absorción y menos retención de agua",
+        price: 50000,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_941662-MCO93053576516_092025-F.webp",
+        nutritionImage: "https://cdnx.jumpseller.com/alma-fit-nutrition/image/46042612/resize/1400/1400?1732553932",
+        category: "creatinas",
+        benefits: [
+            "Mejor absorción",
+            "Sin retención de agua",
+            "Resultados rápidos"
+        ]
+    },
+    {
+        id: "crea_12",
+        name: "Creatina HCL",
+        description: "Mayor absorción y menos retención de agua",
+        price: 34.99,
+        image: "https://m.media-amazon.com/images/I/71f+UBXh2vL._AC_SL1500_.jpg",
+        nutritionImage: "",
+        category: "creatinas",
+        benefits: [
+            "Mejor absorción",
+            "Sin retención de agua",
+            "Resultados rápidos"
+        ]
+    },
+
 ];
--- products/proteinas.js
+++ products/proteinas.js
@@ -59,5 +59,244 @@ const proteinProducts = [
         ],
         flavors: ["CHOCOLATE", "VAINILLA", "FRESA", "COOKIES & CREAM"]
     },
-    
+    {
+        id: "prot_5",
+        name: "BI-PRO CLASSIC 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 155000,
+        image: "https://suplementos.b-cdn.net/proteinas/bi-pro-classic-2lbs-vainilla-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales%20600x800/bi-pro-2lbs-vainilla-tabla-600x800.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["CHOCOLATE", "VAINILLA", "NATURAL"]
+    },
+    {
+        id: "prot_6",
+        name: "BEST PROTEIN 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 180000,
+        image: "https://suplementos.b-cdn.net/proteinas/best-2lb-vainilla-600x800.webp",
+        nutritionImage: "https://evolutionfitness.co/wp-content/uploads/2022/08/BEST-PROTEIN_002.jpg",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["CHOCOLATE GOURMET", "VAINILLA", "FRESA"]
+    },
+    {
+        id: "prot_7",
+        name: "BEST WHEY 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 140000,
+        image: "https://suplementos.b-cdn.net/proteinas/best-whey-vainilla-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/best-whey-tabla-500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["AREQUIPE", "VAINILLA"]
+    },
+    {
+        id: "prot_8",
+        name: "BEST VEGAN 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 120000,
+        image: "https://suplementos.b-cdn.net/proteinas/best-vegan-vainilla-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/best-vegan-500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["CHOCOLATE", "VAINILLA"]
+    },
+    {
+        id: "prot_9",
+        name: "NITRO TECH 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 180000,
+        image: "https://suplementos.b-cdn.net/proteinas/nitro-tech-2lbs-vainilla-cream-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/nitrotech_vanilla_500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["CHOCOLATE", "VAINILLA"]
+    },
+    {
+        id: "prot_10",
+        name: "WHEY GOLD STANDARD 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 185000,
+        image: "https://suplementos.b-cdn.net/proteinas/whey-gold-2lb-vainilla-ice-creme-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/whey-gold-vainilla-tabla-500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["CHOCOLATE", "VAINILLA"]
+    },
+    {
+        id: "prot_11",
+        name: "MASS TECH 6 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 230000,
+        image: "https://suplementos.b-cdn.net/proteinas/mass-tech-6lbs-extreme-vainilla-600x800.webp",
+        nutritionImage: "https://glowell.com.co/cdn/shop/files/extreme-tabla.jpg?v=1753642226&width=990",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["VAINILLA"]
+    },
+    {
+        id: "prot_12",
+        name: "SMART PROTEIN 3 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 85000,
+        image: "https://suplementos.b-cdn.net/proteinas/smart-3lb-vainilla-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/smart-gainer-13lb-tabla-500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["CHOCOLATE", "VAINILLA", "FRESA"]
+    },
+    {
+        id: "prot_13",
+        name: "MEGAPLEX CREATINE 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 60000,
+        image: "https://suplementos.b-cdn.net/proteinas/megaplex-creatine-2lbs-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/megaplex-creatine-power-2lbs-tabla-500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["VAINILLA"]
+    },
+    {
+        id: "prot_14",
+        name: "BI-PRO CLASSIC 3 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 198000,
+        image: "https://suplementos.b-cdn.net/proteinas/bi-pro-3lbs-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales%20600x800/bi-pro-2lbs-vainilla-tabla-600x800.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["VAINILLA"]
+    },
+    {
+        id: "prot_15",
+        name: "PANCAKE DORADO TRADICIONAL",
+        description: "Recuperación muscular y crecimiento",
+        price: 45000,
+        image: "https://suplementos.b-cdn.net/snacks/pancake-dorado-tradicional-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/pancake-upn-tradicional-tabla-500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["TRADICIONAL"]
+    },
+    {
+        id: "prot_16",
+        name: "PANCAKE DORADO CHOCOLATE",
+        description: "Recuperación muscular y crecimiento",
+        price: 55000,
+        image: "https://suplementos.b-cdn.net/snacks/pancake-rojo-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/pancake-upn-500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["TRADICIONAL"]
+    },
+    {
+        id: "prot_17",
+        name: "SERIOUS MASS 6 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 200000,
+        image: "https://suplementos.b-cdn.net/proteinas/serious-mass-6lbs-vainilla-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales%20600x800/serious-mass-12lbs-vainilla-tabla-600x800.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["VAINILLA"]
+    },
+    {
+        id: "prot_18",
+        name: "ISOLATE BASIC 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 170000,
+        image: "https://suplementos.b-cdn.net/proteinas/Isolate-basic-2lbs-vainilla-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales%20600x800/whey-basic-protein-vainilla-tabla-600x800.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+    flavors: ["CHOCOLATE", "SALTED CARAMEL"]
+    },
+    {
+        id: "prot_19",
+        name: "CARNIVOR 2 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 170000,
+        image: "https://suplementos.b-cdn.net/proteinas/carnivor-2lbs-chocolate-600x800.webp",
+        nutritionImage: "https://suplementos.b-cdn.net/tablas%20nutricionales/carnivor-2lbs-chocolate-tabla-500.webp",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+        flavors: ["VAINILLA"]
+    },
+    {
+        id: "prot_20",
+        name: "WHEY PURE 5 LB",
+        description: "Recuperación muscular y crecimiento",
+        price: 250000,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_855944-MCO89680761662_082025-F.webp",
+        nutritionImage: "https://www.smartnutritioncolombia.com/cdn/shop/files/smart_nutrition_linea-whey_wheypure5lb_fresa-tablanutricional.jpg?v=1754404169&width=713",
+        category: "proteinas",
+        benefits: [
+            "Alta concentración de proteína",
+            "Rápida absorción",
+            "Ideal post-entrenamiento"
+        ],
+    flavors: ["CHOCOLATE", "VAINILLA"]
+    },
 ];
